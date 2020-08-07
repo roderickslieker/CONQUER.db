c67e765d77554b12cb3e5d1223f6895d2ab505fc
Roderick Slieker <roderick.slieker@gmail.com>
2020-08-07 13:21:00 +0200

    Updated with sQTLs, lQTLs, mQTLs,

---
+++ R/lQTLs.R
@@ -0,0 +1,9 @@
+#' Data frame of lQTLs
+#'
+#' Author: R. Tabassum
+#' Title: Genetic architecture of human plasma lipidome and its link to cardiovascular disease
+#' Journal: NATURE COMMUNICATIONS | https://doi.org/10.1038/s41467-019-11954-8
+#'
+#' @format A [[data.frame]] object
+#' @source [lQTLs](see description)
+"lqtls"
---
+++ R/sQTLs1.R
@@ -0,0 +1,7 @@
+#' Data frame of sQTLs
+#' 
+#' GTEx V8 sQTLs.
+#'
+#' @format A [[data.frame]] object
+#' @source [sQTLs](see description)
+"sqtls1"
